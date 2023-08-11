aaa36cbe811d965fd569231b3c3ab8d250aa049f
sid-tec <sid.tec.rio@gmail.com>
2023-08-11 11:48:27 -0300

    hoje

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  getHello() {
+    return {
+      nome: "Sedinir"
+    };
+  }
+}
+/* 
+@Injectable()
+export class AppService {
   getHello(): string {
     return 'Hello SID';
   }
-}
+} */
